2ceee5f615e73aa847d19a2abefe6d7f182d7dc5
Arvin Pourabdollah <Arvinpourabdollah@gmail.com>
2023-10-17 10:21:46 +1100

    commit 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import QuoteDisplay from './components/QuoteDisplay';
+import UserInputForm from './components/UserInputForm';
+import LifeCalendar from './components/LifeCalendar';
+import Navbar from './components/Navbar';
+import './styles.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="app">
+        <Navbar />
+
+        <Route path="/" exact>
+          <QuoteDisplay />
+          <UserInputForm />
+        </Route>
+
+        <Route path="/life-calendar">
+          <LifeCalendar />
+        </Route>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
