a91befde9c59742bd169e70ba1fc72f69b3db909
Arvin Pourabdollah <Arvinpourabdollah@gmail.com>
2023-10-17 10:33:38 +1100

    App routed

--- src/App.js
+++ src/App.js
@@ -3,25 +3,21 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import QuoteDisplay from './components/QuoteDisplay';
 import UserInputForm from './components/UserInputForm';
 import LifeCalendar from './components/LifeCalendar';
+import About from './Components/About'
 import Navbar from './components/Navbar';
 import './styles.css';
 
 const App = () => {
   return (
-    <Router>
-      <div className="app">
-        <Navbar />
-
-        <Route path="/" exact>
-          <QuoteDisplay />
-          <UserInputForm />
-        </Route>
-
-        <Route path="/life-calendar">
-          <LifeCalendar />
-        </Route>
-      </div>
-    </Router>
+    <div className="app">
+      <Navbar/>
+      <Router>
+        <Route path="/" exact Component={QuoteDisplay}/>
+        <Route path='/user-info' Component={UserInputForm}/>
+        <Route path='/life-calendar' Component={LifeCalendar}/>
+        <Route path='/about' Component={About}/>
+      </Router>
+    </div>
   );
 };
 
