6486eb8eb31986002c10569e23457767474ac616
Arvin Pourabdollah <Arvinpourabdollah@gmail.com>
2023-10-17 13:26:19 +1100

    db json

---
+++ src/api/server.js
@@ -0,0 +1,22 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router({
+  lifeQuotes,
+  userInfo
+});
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3001;
+
+server.use(middlewares);
+server.use(jsonServer.bodyParser);
+
+// Define custom routes (if needed)
+server.get('/customRoute', (req, res) => {
+  res.json({ customData: 'Hello from custom route!' });
+});
+
+server.use('/api', router); // Base URL for API endpoints
+
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
