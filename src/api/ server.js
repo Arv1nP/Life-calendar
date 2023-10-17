7b53fabf157fbe5ffb978cd9c8447872fcbc9d9a
Arvin Pourabdollah <Arvinpourabdollah@gmail.com>
2023-10-17 12:51:25 +1100

    Quote data set

---
+++ src/api/ server.js
@@ -0,0 +1,52 @@
+const lifeQuotes = [
+    {
+        id: 1,
+        quote: "Every day may not be good, but there's something good in every day.",
+        author: "Unknown"
+      },
+      {
+        id: 2,
+        quote: "The purpose of our lives is to be happy.",
+        author: "Dalai Lama"
+      },
+      {
+        id: 3,
+        quote: "Life is short, and it's up to you to make it sweet.",
+        author: "Sarah Louise Delany"
+      },
+      {
+        id: 4,
+        quote: "Life is a gift, and it offers us the privilege, opportunity, and responsibility to give something back by becoming more.",
+        author: "Tony Robbins"
+      },
+      {
+        id: 5,
+        quote: "Life is change, growth is optional. Choose wisely.",
+        author: "Karen Kaiser Clark"
+      },
+      {
+        id: 6,
+        quote: "The future belongs to those who believe in the beauty of their dreams.",
+        author: "Eleanor Roosevelt"
+      },
+      {
+        id: 7,
+        quote: "The best time to plant a tree was 20 years ago. The second best time is now.",
+        author: "Chinese Proverb"
+      },
+      {
+        id: 5,
+        quote: "Don't watch the clock; do what it does. Keep going.",
+        author: "Sam Levenson"
+      },
+      {
+        id: 9,
+        quote: "You are never too old to set another goal or to dream a new dream.",
+        author: "C.S. Lewis"
+      },
+      {
+        id: 10,
+        quote: "The only limit to our realization of tomorrow will be our doubts of today.",
+        author: "Franklin D. Roosevelt"
+      }
+  ];
