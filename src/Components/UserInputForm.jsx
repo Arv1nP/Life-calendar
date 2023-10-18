d261da1bdc51afaa6199e46c79a885b31dd7556c
Arvin Pourabdollah <Arvinpourabdollah@gmail.com>
2023-10-18 16:35:06 +1100

    user input post

--- src/Components/UserInputForm.jsx
+++ src/Components/UserInputForm.jsx
@@ -1,41 +1,78 @@
-import { useState } from "react"
+import React, { useState } from "react";
 
-export default function UserInputForm(){
-    const [userInput, setUserInput]= useState({
-        firstName: "",
-        lastName: "",
-        DOB: {DD,MM,YY},
-        lifeLengthGuess: ""
-    });
-    function handleUserInput(e){
-        setUserInput({...userInput, [e.target.name]: e.target.value});
-        console.log([e.target.name], e.target.value)        
-    };
-    function handleSubmit(e){
-        e.preventDefault();
-        console.log(userInput)
-    };
+export default function UserInputForm() {
+  const [userInput, setUserInput] = useState({
+    firstName: "",
+    lastName: "",
+    day: "",
+    month: "",
+    year: "",
+    lifeLengthGuess: ""
+  });
 
+  function handleUserInput(e) {
+    const { name, value } = e.target;
+    setUserInput({ ...userInput, [name]: value });
+  }
 
-    return(
-        <section className="user-input-form">
-          <h2>Lets Personalise</h2>
-          <form  onSubmit={handleSubmit}>
-            <label>Your first name
-                <input type="text" name="firstName" value={userInput.firstName} onChange={handleUserInput}/>
-            </label>
-            <label>Your last name
-                <input type="text" name="lastName" value={userInput.lastName} onChange={handleUserInput}/>
-            </label>
-            <label>Your date of birth
-                <input type="text" placeholder="DD" name="DOB" value={userInput.DOB.DD} onChange={handleUserInput}/>
-                <input type="text" placeholder="MM" name="DOB" value={userInput.DOB.MM} onChange={handleUserInput}/>
-                <input type="text" placeholder="YY" name="DOB" value={userInput.DOB.YY} onChange={handleUserInput}/>
-            </label>
-            <label>How long you think you would live
-                <input type="text" placeholder="Years" name="lifeLengthGuess" value={userInput.lifeLengthGuess} onChange={handleUserInput}/>
-            </label>
-          </form>
-        </section>
-    )
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetch('http://localhost:3000/api/user-info', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(userInput),
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log('Success:', data);
+        // Handle success, update state, etc.
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+        // Handle errors
+      });
+  };
+
+  const { firstName, lastName, day, month, year, lifeLengthGuess } = userInput;
+
+  return (
+    <section className="user-input-form">
+      <h2>Lets Personalise</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Your first name
+          <input
+            type="text" name="firstName" value={firstName} onChange={handleUserInput}
+          />
+        </label>
+        <label>
+          Your last name
+          <input
+            type="text" name="lastName" value={lastName} onChange={handleUserInput}
+          />
+        </label>
+        <label>
+          Your date of birth
+          <input
+            type="text" placeholder="DD" name="day" value={day} onChange={handleUserInput}
+          />
+          <input
+            type="text" placeholder="MM" name="month" value={month} onChange={handleUserInput}
+          />
+          <input
+            type="text" placeholder="YY" name="year" value={year} onChange={handleUserInput}
+          />
+        </label>
+        <label>
+          How long you think you would live
+          <input
+            type="text" placeholder="Years" name="lifeLengthGuess" value={lifeLengthGuess} onChange={handleUserInput}
+          />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+    </section>
+  );
 }
