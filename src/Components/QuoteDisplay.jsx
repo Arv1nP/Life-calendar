165cfb9265ded4a1e96b3f9df40637d06cf2fb29
Arvin Pourabdollah <Arvinpourabdollah@gmail.com>
2023-10-18 14:01:54 +1100

    just a single quote

--- src/Components/QuoteDisplay.jsx
+++ src/Components/QuoteDisplay.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 
 export default function QuoteDisplay() {
   const [quotes, setQuotes] = useState([]);
+  const [randomIndex, setRandomIndex] = useState(null)
+
 
   useEffect(() => {
     async function fetchQuotes() {
@@ -9,6 +11,8 @@ export default function QuoteDisplay() {
         const res = await fetch('http://localhost:3001/api/lifeQuotes');
         const data = await res.json();
         setQuotes(data);
+        const index = Math.floor(Math.random() * data.length);
+        setRandomIndex(index)
       } catch (error) {
         console.error('Error fetching quotes:', error);
       }
@@ -16,15 +20,19 @@ export default function QuoteDisplay() {
 
     fetchQuotes();
   }, []); 
+  
 
   return (
-    <div>
-      {quotes.map((quote) => (
-        <div key={quote.id}>
-          <p>{quote.quote}</p>
-          <p>- {quote.author}</p>
-        </div>
-      ))}
+    <div className="quotedisplay">
+      {quotes
+        .filter(quote => quote.id === randomIndex)
+        .map(quote => (
+          <div key={quote.id}>
+            <p>{quote.quote}</p>
+            <p>- {quote.author}</p>
+          </div>
+        ))
+      }
     </div>
   );
 }
