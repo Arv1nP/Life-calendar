7579bf8c5ebc42aed83777e1ddf90d1433b26860
Arvin Pourabdollah <Arvinpourabdollah@gmail.com>
2023-10-18 11:13:26 +1100

    Quotes update

--- src/Components/QuoteDisplay.jsx
+++ src/Components/QuoteDisplay.jsx
@@ -1,16 +1,30 @@
-import { useEffect } from "react"
+import React, { useState, useEffect } from "react";
 
-export default function QuoteDisplay(){
+export default function QuoteDisplay() {
+  const [quotes, setQuotes] = useState([]);
 
-  const [quotes, setQuotes] = useEffect("")
-  async function fetchQuotes(){
-    const res = await fetch('http://localhost:3001/api/lifeQuotes')
-    const data = await res.json()
-    console.log(data)
-    setQuotes(data)
-  }
-  
-  return(
-    <div></div>
-  )
+  useEffect(() => {
+    async function fetchQuotes() {
+      try {
+        const res = await fetch('http://localhost:3001/api/lifeQuotes');
+        const data = await res.json();
+        setQuotes(data);
+      } catch (error) {
+        console.error('Error fetching quotes:', error);
+      }
+    }
+
+    fetchQuotes();
+  }, []); 
+
+  return (
+    <div>
+      {quotes.map((quote) => (
+        <div key={quote.id}>
+          <p>{quote.quote}</p>
+          <p>- {quote.author}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
