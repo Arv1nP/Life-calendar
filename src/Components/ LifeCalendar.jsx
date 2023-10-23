d260c0dda0234f220448146a84778ffeaac6cac9
Arvin Pourabdollah <Arvinpourabdollah@gmail.com>
2023-10-23 11:33:38 +1100

    lifeCalendar skeleton

--- src/Components/ LifeCalendar.jsx
+++ src/Components/ LifeCalendar.jsx
@@ -1,6 +1,17 @@
 export default function LifeCalendar(userInput){
   const { firstName, lastName, day, month, year, lifeLengthGuess } = userInput;
-    return(
-      <div></div>
-    )
-  }
+  const totalWeeks = [lifeLengthGuess * 56];
+  
+
+  return (
+    <div className="lifeGrid">
+      <div className="row">
+        {totalWeeks.map((week) => (
+          <div key={week} className="week">
+            {week}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
